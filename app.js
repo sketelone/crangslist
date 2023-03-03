906e1a7a22ba6c18430ddc1fdf655682d4b05423
Sabrina <sketelone@gmail.com>
2023-03-03 12:53:04 -0800

    fix favicon

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
@@ -26,6 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
